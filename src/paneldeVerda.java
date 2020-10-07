80e80e6e4374e125a672d1ba8966fefc8c26e988
daguilerap <56959256+daguilerap@users.noreply.github.com>
2020-10-07 19:08:51 +0200

    Ver 3

--- src/paneldeVerda.java
+++ src/paneldeVerda.java
@@ -10,22 +10,31 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class paneldeVerda extends Component {
+    private static String dir;
+
     public static void main(String[] args) {
         JFrame marco = new JFrame("Puntuable AD");
+
         marco.setSize(600, 400);
         marco.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         marco.setLayout(new BorderLayout(10, 0));
         JTabbedPane pestañas = new JTabbedPane();
+        JPanel panel3 = new JPanel();
+        JPanel panel4 = new JPanel();
+        JPanel panel5 = new JPanel();
 
         JPanel panel1 = new JPanel();
+        JPanel crear = new JPanel();
         JButton btn = new JButton("Buscar...");
         JButton btn2 = new JButton("Crear");
         JButton btn3 = new JButton("Mostrar");
         JButton btn4 = new JButton("Borrar");
+        JButton btn5 = new JButton("Crea Texto");
         panel1.add(btn);
         panel1.add(btn2);
         panel1.add(btn3);
         panel1.add(btn4);
+
 
 
         JPanel panel2 = new JPanel();
@@ -35,11 +44,17 @@ public class paneldeVerda extends Component {
         JTextField txt3 = new JTextField(20);
         JTextField txt4 = new JTextField(20);
         JTextField txt5 = new JTextField(20);
+        JTextArea contenido_texto= new JTextArea();
+
+        JTextField nombrearchivo = new JTextField(25);
         JLabel lab= new JLabel("Nombre: ");
         JLabel lab2 = new JLabel("Formato: ");
         JLabel lab3 = new JLabel("Ruta:     ");
         JLabel lab4 = new JLabel("Tamaño: ");
         JLabel lab5 = new JLabel("Última modificación: ");
+        JLabel nombre_archiv_crear = new JLabel("Nombre del archivo");
+
+
         panel2.add(lab);
         panel2.add(txt);
         panel2.add(lab2);
@@ -51,14 +66,28 @@ public class paneldeVerda extends Component {
         panel2.add(lab5);
         panel2.add(txt5);
 
+
+
         pestañas.addTab("Panel 1",panel1);
         pestañas.addTab("Panel 2",panel2);
+        panel1.add(panel3);
+        panel1.add(panel4);
+        panel4.add(panel5);
+
+
 
         marco.add(pestañas);
-        pestañas.add(panel1);
-        pestañas.add(panel2);
+   
+        contenido_texto.setPreferredSize(new Dimension(300,200));
 
+        panel3.add(nombre_archiv_crear);
+        panel3.add(nombrearchivo);
+        panel5.add(btn5);
+        panel4.add(contenido_texto);
         marco.setVisible(true);
+        panel3.setVisible(false);
+        panel4.setVisible(false);
+        panel5.setVisible(false);
         btn.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
                 JFileChooser fileChooser = new JFileChooser();
@@ -84,7 +113,56 @@ public class paneldeVerda extends Component {
                 }
             }
         });
+/////crear archivo
+        btn2.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                JFileChooser fileChooser = new JFileChooser();
+                fileChooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+
+                int seleccion = fileChooser.showOpenDialog(crear);
+
+
+
+                if (seleccion == JFileChooser.APPROVE_OPTION)
+                {
+                    File directorio = fileChooser.getSelectedFile();
+
+                    panel3.setVisible(true);
+                    panel4.setVisible(true);
+                    panel5.setVisible(true);
+
+
+
+
+
+
+                    btn5.addActionListener(new ActionListener(){
+                    public void actionPerformed(ActionEvent e) {
+                        String nombre = nombrearchivo.getText();
+                        String direccion = directorio.getAbsolutePath();
+                        String texto=contenido_texto.getText();
+
+                        creaTexto texto1 = new creaTexto();
+                        dir=(texto1.creandoTexto(nombre,direccion,texto));
+
+                        panel3.setVisible(false);
+                        panel4.setVisible(false);
+                        panel5.setVisible(false);
+
+
+
+                    }
+
+                });
+
+                }
+
+            }
+        });
     }
+
+
+
     public static String format(long time) {
         DateFormat sdf = new SimpleDateFormat("dd-MM-yyyy HH:mm:ss"); return sdf.format(new Date(time));
     }
